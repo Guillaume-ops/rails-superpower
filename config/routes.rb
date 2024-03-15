9cfc97ae2c307838bf06a6a3e7e27877acc2eba9
Guillaume Falandry <guillaumefalandry@gmail.com>
2024-03-15 15:48:23 +0100

    failed

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show], as: :dashboard
   resources :bookings, only: [:destroy]
   resources :users, only: [:destroy]
+  resources :superpowers, only: [:destroy]
 end
